ab3359baccfc187c96fcae9b467800dac4bb7798
Alfred M <alfred.mkg@proton.me>
2023-09-28 20:09:59 +0300

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,4 +7,11 @@ describe Solver do
       expect(solver).to be_an_instance_of(Solver)
     end
   end
+
+  describe '#factorial' do
+    it 'takes an integeter and returns the factorial of that number' do
+      solver = Solver.new()
+      expect(solver.factorial(10)).to be(3_628_800)
+    end
+  end
 end
