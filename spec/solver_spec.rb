1925ebbaff143872bf1bc7c6964cede21386d092
Alfred M <alfred.mkg@proton.me>
2023-09-28 20:19:32 +0300

    Add edge case test for factorial of 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,15 +3,20 @@ require './solver'
 describe Solver do
   describe '#new' do
     it 'creates a new Solver object' do
-      solver = Solver.new()
+      solver = Solver.new
       expect(solver).to be_an_instance_of(Solver)
     end
   end
 
   describe '#factorial' do
     it 'takes an integeter and returns the factorial of that number' do
-      solver = Solver.new()
+      solver = Solver.new
       expect(solver.factorial(10)).to be(3_628_800)
+    end
+
+    it 'returns 0 if the integer passed is 0' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to be(0)
     end
   end
 end
