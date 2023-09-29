d141c8a9bdae17679ce8371de14fd395f09bc697
Henok Kirubel <henokhm2@gmail.com>
2023-09-29 09:30:08 +0300

    Fix rubocop offences

--- solver.rb
+++ solver.rb
@@ -26,11 +26,11 @@ class Solver
   end
 
   def fizzbuzz(int)
-    if int % 3 == 0 && int % 5 == 0
+    if (int % 3).zero? && (int % 5).zero?
       'fizzbuzz'
-    elsif int % 3 == 0
+    elsif (int % 3).zero?
       'fizz'
-    elsif int % 5 == 0
+    elsif (int % 5).zero?
       'buzz'
     else
       int.to_s
